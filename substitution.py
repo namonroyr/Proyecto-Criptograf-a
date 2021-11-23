cb9b0d2c4d96d715e822ff4554a6cc6290a5aa50
david <drpedrazas@unal.edu.co>
2021-11-22 23:30:59 -0500

    Agregando datos adicionales

--- substitution.py
+++ substitution.py
@@ -4,7 +4,16 @@ import string
 
 
 class substitution:
-    ALPHABET = {}
+    freqs = [.082, .015, .028, .043, .127, .022, .020, .061, .070, .002, .008, .040, .024, .067, .075, .019, .001, .060,
+             .063, .091, .028, .010, .023, .001, .020, .001]
+    ALPHABETfreq = {string.ascii_lowercase[i]: freqs[i] for i in range(26)}
+
+    DIGRAMAS = {'he': 0.0128, 'th': 0.0152, 'in': 0.0094, 'er': 0.0094, 'an': 0.0084, 're': 0.0064, 'nd': 0.0063,
+                'at': 0.0059, 'on': 0.0057, 'nt': 0.0056, 'ha': 0.0056, 'es': 0.0056, 'st': 0.0055, 'en': 0.0055,
+                'ed': 0.0053, 'to': 0.0052}
+    TRIGRAMAS = {'the': 0.0181, 'and': 0.0073, 'tha': 0.0033, 'ent': 0.0042, 'ing': 0.007, 'ion': 0.0042, 'tio': 0.0031,
+                 'for': 0.0034}
+
     def __init__(self, text: str):
         self.text = text
         self.work_text = "".join([i for i in text.lower() if i in string.ascii_lowercase])
@@ -12,16 +21,16 @@ class substitution:
         self.freq_mono = {u: self.work_text.count(u) for u in set(self.work_text)}
         self.freq_di = {u: self._digrams.count(u) for u in set(self._digrams)}
         self.freq_tri = {u: self._trigrams.count(u) for u in set(self._trigrams)}
-        self.key = {u : u for u in string.ascii_lowercase}
+        self.key = {u: u for u in string.ascii_lowercase}
         self.keychain = [self.key]
 
     @property
     def _digrams(self):
-        return [self.work_text[i:i+2] for i in range(len(self.work_text)-2)]
+        return [self.work_text[i:i + 2] for i in range(len(self.work_text) - 2)]
 
     @property
     def _trigrams(self):
-        return [self.work_text[i:i+3] for i in range(len(self.work_text)-3)]
+        return [self.work_text[i:i + 3] for i in range(len(self.work_text) - 3)]
 
     def permutar(self, tabla: dict):
         if tabla != self.key:
@@ -38,6 +47,4 @@ class substitution:
             self.permutar(self.key)
 
     def invert(self):
-        self.permutar({self.key[u]:u for u in self.key})
-
-
+        self.permutar({self.key[u]: u for u in self.key})
