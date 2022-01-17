7a9ade42dea9f70931e28feacef7b91a87f51c2d
David Ricardo Pedraza Silva <63820927+drpedrazas@users.noreply.github.com>
2022-01-17 03:59:56 -0500

    generate permutation

--- SDES.py
+++ SDES.py
@@ -6,7 +6,15 @@ IP = [2, 6, 3, 1, 4, 8, 5, 7 ]
 EP = [ 4, 1, 2, 3, 2, 3, 4, 1 ]
 P4 = [ 2, 4, 3, 1 ]
 IP_inv = [ 4, 1, 3, 5, 7, 2, 8, 6 ]
+S0 = [ [ 1, 0, 3, 2 ],
+    [ 3, 2, 1, 0 ],
+    [ 0, 2, 1, 3 ],
+    [ 3, 1, 3, 2 ] ]
 
+S1 = [ [ 0, 1, 2, 3 ],
+     [ 2, 0, 1, 3 ],
+    [ 3, 0, 1, 0 ],
+    [ 2, 1, 0, 3 ] ]
 def key_generation():
     key_prime = [key[P10[i]] for i in range(10)]
     Ls = key_prime[:5]
@@ -20,6 +28,23 @@ def key_generation():
     key_prime = Ls_2 + Rs_2
     key2 = [key_prime[P8[i]] for i in range(8)]
     return key1, key2
+
+
+def shift(lista, n):
+    while n > 0:
+        temp = lista[0]
+        for i in range(len(lista)-1):
+            lista[i] = lista[i+1]
+        lista[-1] = temp
+        n -= 1
+    return lista
+
+def funcion(lista, llave):
+    l = list() * 4
+    r = list() * 4
+    for i in range(4):
+        l[i] = lista[i]
+        r[i] = lista[i+4]
 
 
 def Function(arr,key):
